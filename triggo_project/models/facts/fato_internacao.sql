d92eee818f5761d1a930f7c436bc72f275566ef8
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-11 22:30:56 -0400

    [refactor] Refatorando a minha fato_interncao

--- triggo_project/models/facts/fato_internacao.sql
+++ triggo_project/models/facts/fato_internacao.sql
@@ -2,12 +2,12 @@ select
     numero_aih,
     ano_competencia,
     mes_competencia,
-    data_internacao,
-    data_saida,
-    dias_permanencia,
-    valor_total,
-    cid_principal,
-    cid_secundario,
-    cnpj_hospital,
-    municipio_residencia
+    to_date(dt_inter, 'YYYYMMDD') as data_internacao,
+    to_date(dt_saida, 'YYYYMMDD') as data_saida,
+    dias_perm as dias_permanencia,
+    val_tot as valor_total,
+    diag_princ as cid_principal,
+    diag_secun as cid_secundario,
+    cgc_hosp as cnpj_hospital,
+    munic_res as municipio_residencia
 from {{ ref('stg_aih') }}
