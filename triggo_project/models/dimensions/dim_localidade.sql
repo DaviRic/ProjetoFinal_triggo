1f0b49b2963a9c768ba27aff70463b338936d1f5
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-11 22:04:22 -0400

    [fix] Corrigindo typo e adicionando a lógica final da consulta

--- triggo_project/models/dimensions/dim_localidade.sql
+++ triggo_project/models/dimensions/dim_localidade.sql
@@ -1,8 +1,13 @@
 with base as (
     select distinct
-        minucipio_residencia as municipio,
+        municipio_residencia as municipio,
         uf as estado
     from {{ ref('stg_aih')}}
     where municipio_residencia is not null
 )
+
 select
+    row_number() over (order by municipio, estado) as id_localidade,
+    municipio,
+    estado
+from base
