dd3583915325f4ccc0fbb8f656e7b51e483d8e0f
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-12 23:24:59 -0400

    [refactor] Refatoração da dimensão tempo

--- triggo_project/models/dimensions/dim_tempo.sql
+++ triggo_project/models/dimensions/dim_tempo.sql
@@ -1,21 +1,26 @@
 with date_range as (
     select
-        dateadd(day, seq4(), '2024-01-01') as data
-    from table(generator(rowcount => 366)) 
+        dateadd(day, seq4(), '2000-01-01') as data
+    from table(generator(rowcount => 10958)) 
 ),
 dim_tempo as (
     select
+        to_char(data, 'YYYYMMDD') as id_data,
         data,
         extract(year from data) as ano,
-        extract(month from data) as mes,
+        extract(month from data) as mes_num,
+        monthname(data) as mes_nome,
         extract(day from data) as dia,
+        dayname(data) as dia_da_semana,
         extract(quarter from data) as trimestre,
-        to_char(data, 'YYYY-MM-DD') as data_formatada,
-        dayofweek(data) as dia_da_semana,
         case
             when dayofweek(data) in (1,7) then 'Fim de Semana'
             else 'Dia Útil'
-        end as tipo_dia
+        end as tipo_dia,
+        case
+            when data = current_date() then true
+            else false
+        end as e_dia_atual
     from date_range
 )
 
