2f49a2d92ec43b4df54108d807e7dba76d933b5a
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-12 22:30:26 -0400

    [refactor] Refatoração da dim_localidade

--- triggo_project/models/dimensions/dim_localidade.sql
+++ triggo_project/models/dimensions/dim_localidade.sql
@@ -1,11 +1,36 @@
-with base as (
+with stg_aih as (
     select distinct
-        municipio_residencia as municipio_id,
-        uf as uf_zi
+        municipio_residencia_id,
+        municipio_movimentacao_id,
+        uf
     from {{ ref('stg_aih') }}
+),
+
+todas_localidades as (
+    select
+        municipio_residencia_id as municipio_id,
+        uf
+    from stg_aih
+    where municipio_residencia_id is not null
+    
+    union
+    
+    select
+        municipio_movimentacao_id as municipio_id,
+        uf
+    from stg_aih
+    where municipio_movimentacao_id is not null
+),
+
+final as (
+    select distinct
+        municipio_id,
+        uf as uf_sigla
+    from todas_localidades
 )
 
 select
+    {{ dbt_utils.generate_surrogate_key(['municipio_id']) }} as id_localidade,
     municipio_id,
-    uf_zi
-from base
+    uf_sigla
+from final
