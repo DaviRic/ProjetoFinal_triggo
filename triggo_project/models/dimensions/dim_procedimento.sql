b43ed491b73f31cb3c1d630592daa925c77bebc0
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-12 22:49:26 -0400

    [refactor] Refatorando a dim_procedimento para dados mais consistentes

--- triggo_project/models/dimensions/dim_procedimento.sql
+++ triggo_project/models/dimensions/dim_procedimento.sql
@@ -1,25 +1,34 @@
 with base as (
     select distinct
-        proc_solic as procedimento_solicitado,
-        proc_rea as procedimento_realizado
+        procedimento_solicitado,
+        procedimento_realizado
     from {{ ref('stg_aih') }}
 ),
 
-PROC_SOLIC as (
-    select distinct procedimento_solicitado as procedimento, 'Solicitado' as tipo from base where procedimento_solicitado is not null
+proc_solicitado as (
+    select distinct
+        procedimento_solicitado as procedimento_codigo,
+        'Solicitado' as tipo
+    from base
+    where procedimento_solicitado is not null
 ),
 
-PROC_REA as (
-    select distinct procedimento_realizado as procedimento, 'Realizado' as tipo from base where procedimento_realizado is not null
+proc_realizado as (
+    select distinct
+        procedimento_realizado as procedimento_codigo,
+        'Realizado' as tipo
+    from base
+    where procedimento_realizado is not null
 ),
 
 proc_union as (
-    select * from proc_solic
+    select * from proc_solicitado
     union
-    select * from PROC_REA
+    select * from proc_realizado
 )
 
 select
-    procedimento,
+    {{ dbt_utils.generate_surrogate_key(['procedimento_codigo', 'tipo']) }} as id_procedimento,
+    procedimento_codigo,
     tipo
 from proc_union
