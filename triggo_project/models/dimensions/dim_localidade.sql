b9140ed67060de8c8b3675edc604e137d5cefa5e
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-13 19:47:22 -0400

    [refactor] Corrigindo a dim_localidade para tratar inconsistências

--- triggo_project/models/dimensions/dim_localidade.sql
+++ triggo_project/models/dimensions/dim_localidade.sql
@@ -1,36 +1,35 @@
 with stg_aih as (
     select distinct
         municipio_residencia_id,
-        municipio_movimentacao_id,
-        uf
+        municipio_movimentacao_id
     from {{ ref('stg_aih') }}
 ),
 
 todas_localidades as (
     select
-        municipio_residencia_id as municipio_id,
-        uf
+        municipio_residencia_id as municipio_id
     from stg_aih
     where municipio_residencia_id is not null
     
     union
     
     select
-        municipio_movimentacao_id as municipio_id,
-        uf
+        municipio_movimentacao_id as municipio_id
     from stg_aih
     where municipio_movimentacao_id is not null
 ),
 
 final as (
     select distinct
-        municipio_id,
-        uf as uf_sigla
+        municipio_id
     from todas_localidades
 )
 
 select
     {{ dbt_utils.generate_surrogate_key(['municipio_id']) }} as id_localidade,
     municipio_id,
-    uf_sigla
+    case 
+        when left(municipio_id, 2) = '13' then 'AM' 
+        else 'Outro' 
+    end as uf_sigla
 from final
