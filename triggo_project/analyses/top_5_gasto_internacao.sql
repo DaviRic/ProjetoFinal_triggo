55c9d9596ff5ed6fec6b87d520b657ffb57320c6
DaviCruvel <davi.r.cruvel@gmail.com>
2025-08-14 00:42:08 -0400

    [refactor] Alterando o estado para o certo

--- triggo_project/analyses/top_5_gasto_internacao.sql
+++ triggo_project/analyses/top_5_gasto_internacao.sql
@@ -2,7 +2,7 @@ SELECT
     cid_principal,
     SUM(valor_total_internacoes) AS gasto_total
 FROM {{ ref('mrt_internacoes_mensal') }}
-WHERE estado = 'SP'
+WHERE estado = 'AM'
 GROUP BY 1
 ORDER BY gasto_total DESC
 LIMIT 5;
